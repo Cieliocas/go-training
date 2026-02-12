3c9e29aa954de8602e9d458a59af6284ee8586a3
cieliocas <francielioevangelista@hotmail.com>
2026-02-12 12:45:47 -0300

    Introdução a struct

--- 09-struct/main.go
+++ 09-struct/main.go
@@ -8,6 +8,14 @@ type Pessoa struct {
 	Maior bool
 }
 
+func (p Pessoa) Apresentar() string {
+	return fmt.Sprintf("Olá, meu nome é %s, tenho %d anos.", p.Nome, p.Idade)
+}
+
+func (p *Pessoa) FazerAniversario() {
+	p.Idade++
+}
+
 func main() {
 	pessoa := Pessoa{"Lucas", 30, true}
 	fmt.Println("Nome:", pessoa.Nome)
@@ -17,4 +25,7 @@ func main() {
 	fmt.Println("Pessoa Nome &:", &pessoa.Nome)
 	fmt.Println("Pessoa Idade &:", &pessoa.Idade)
 	fmt.Println("Pessoa Maior &:", &pessoa.Maior)
+	fmt.Println("Lucas diz:", pessoa.Apresentar())
+	pessoa.FazerAniversario()
+	fmt.Println("Lucas fez aniversário! Agora tem", pessoa.Idade)
 }
