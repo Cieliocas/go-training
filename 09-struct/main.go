0f86123d424d5d903470565ccc8bc0dad82871ff
cieliocas <francielioevangelista@hotmail.com>
2026-02-12 12:30:16 -0300

    Introdução a struct

--- 09-struct/main.go
+++ 09-struct/main.go
@@ -11,4 +11,9 @@ func main() {
 	pessoa := Pessoa{"Lucas", 30}
 	fmt.Println("Nome:", pessoa.Nome)
 	fmt.Println("Idade:", pessoa.Idade)
+	fmt.Println("Pessoa:", pessoa)
+	fmt.Println("Pessoa &:", &pessoa)
+	fmt.Println("Pessoa Nome &:", &pessoa.Nome)
+	fmt.Println("Pessoa Idade &:", &pessoa.Idade)
+
 }
