b7560ee1f4b5da16592212fc955f6062116be03a
cieliocas <francielioevangelista@hotmail.com>
2026-02-12 12:33:58 -0300

    Introdução a struct

--- 09-struct/main.go
+++ 09-struct/main.go
@@ -5,15 +5,16 @@ import "fmt"
 type Pessoa struct {
 	Nome  string
 	Idade int
+	Maior bool
 }
 
 func main() {
-	pessoa := Pessoa{"Lucas", 30}
+	pessoa := Pessoa{"Lucas", 30, true}
 	fmt.Println("Nome:", pessoa.Nome)
 	fmt.Println("Idade:", pessoa.Idade)
 	fmt.Println("Pessoa:", pessoa)
 	fmt.Println("Pessoa &:", &pessoa)
 	fmt.Println("Pessoa Nome &:", &pessoa.Nome)
 	fmt.Println("Pessoa Idade &:", &pessoa.Idade)
-
+	fmt.Println("Pessoa Maior &:", &pessoa.Maior)
 }
