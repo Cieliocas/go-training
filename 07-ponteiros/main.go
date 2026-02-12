2e75d3598f78fcf4d0eceb8bfac1d00e132341e1
cieliocas <francielioevangelista@hotmail.com>
2026-02-12 11:54:26 -0300

    Manipulacao de variaveis com ponteiros

--- 07-ponteiros/main.go
+++ 07-ponteiros/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func MainPlus() {
 	idade := 30
 	ponteiroIdade := &idade
 
@@ -10,4 +10,12 @@ func main() {
 	fmt.Println("Endereço de memória da idade:", &idade)
 	fmt.Println("Valor do ponteiro para idade:", ponteiroIdade)
 	fmt.Println("Valor apontado pelo ponteiro:", *ponteiroIdade)
+}
+
+func main() {
+	numero := 42
+	ponteiro := &numero
+	fmt.Println("Antes da alteração:", numero)
+	*ponteiro = 99
+	fmt.Println("Depois da alteração:", numero)
 }
